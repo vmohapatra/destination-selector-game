521b8a2971c9dc07ea831554e9a2cd1f4769d6f6
vmohapatra <vmohapatra@expedia.com>
2016-03-15 14:36:07 -0700

    Updating config

--- data/load.bat
+++ data/load.bat
@@ -1,6 +1,11 @@
 mongo prototype --eval db.imagetags.drop()
+
 mongo prototype --eval db.desttags.drop()
+
 mongo prototype --eval db.imagelocations.drop()
+
 mongoimport -d prototype -c imagetags --type csv --file imageTags.csv --headerline
+
 mongoimport -d prototype -c desttags --type csv --file destTagsClean.csv --headerline
+
 mongoimport -d prototype -c imagelocations --type csv --file imageLocations.csv --headerline
