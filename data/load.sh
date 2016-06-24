de96c7b1d34a54dada68ddfdaa1bd922373c19c1
vmohapatra <vmohapatra@expedia.com>
2016-06-24 13:06:52 -0700

    Adding comments to load file

--- data/load.sh
+++ data/load.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #http://stackoverflow.com/questions/6753330/specify-which-database-to-use-in-mongodb-js-script
 #mongo <name of db> --eval "db.runCommand( <js in here> );"
+#Drop these preexisting tables if they exist
 mongo prototype --eval 'db.imagetags.drop()'
 mongo prototype --eval 'db.desttags.drop()'
 mongo prototype --eval 'db.imagelocations.drop()'
+#import from csv files the preloaded tag data
 mongoimport -d prototype -c imagetags --type csv --file imageTags.csv --headerline
 mongoimport -d prototype -c desttags --type csv --file destTags2.csv --headerline
 mongoimport -d prototype -c imagelocations --type csv --file imageLocations.csv --headerline
