b57406433ac71a009bcba486e7bab7ffaf054b14
vmohapatra <vijaya_126@hotmail.com>
2018-02-27 11:59:18 -0800

    Updating mongoose to fix app in browser issue

--- db.js
+++ db.js
@@ -4,6 +4,13 @@ var mongoose = require('mongoose');
 var config = require('./config');
 mongoose.connect(config.db_url);
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:' + config.db_url));
+db.once('open', function() {
+  // we're connected!
+  console.log('Connected successfully to :' + config.db_url);
+});
+
 module.exports = {
     User: require('./models/user'),
     Game: require('./models/game'),
